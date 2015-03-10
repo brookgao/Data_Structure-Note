fe4f7d205e6dc07e3360082a535860915ec19f4e
AOC1 <brookgao621@outlook.com>
2015-03-10 16:53:26 +0800

    second commit

---
+++ Tree/BinaryTree.cpp
@@ -0,0 +1,9 @@
+//
+//  BinaryTree.cpp
+//  Data_Structure
+//
+//  Created by GaoBrook on 15/3/10.
+//  Copyright (c) 2015年 gao. All rights reserved.
+//
+
+#include "BinaryTree.h"
---
+++ Tree/BinaryTree.h
@@ -0,0 +1,14 @@
+//
+//  BinaryTree.h
+//  Data_Structure
+//
+//  Created by GaoBrook on 15/3/10.
+//  Copyright (c) 2015年 gao. All rights reserved.
+//
+
+#ifndef __Data_Structure__BinaryTree__
+#define __Data_Structure__BinaryTree__
+
+#include <stdio.h>
+
+#endif /* defined(__Data_Structure__BinaryTree__) */
